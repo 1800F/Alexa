b18ca1d780b6404cd99e08db132aabc4354fd6d8
Christijan <christijand@mediarain.com>
2016-03-30 14:26:03 -0600

    revision of arrangement and size descriptions

---
+++ skill/ProductDescriptions.js
@@ -0,0 +1,58 @@
+var descriptions = function () {
+
+  return {
+    Descriptions: {
+
+      MothersEmbraceDescription: {
+        say: "This delicate bouquet, features light pink roses, pink Oriental, Hybrid, and Peruvian lilies, carnations, white snapdragons, pink stock, and salal tips. It is artistically designed in an eight-inch-tall, pink, vintage-inspired glass vase."
+      },
+      MothersEmbraceSmall: {
+        say: "The small arrangement is about 17 inches high, including the eight-inch vase, and it is about 14 inches in diameter."
+      },
+      MothersEmbraceMedium: {
+        say: "The medium arrangement is about 18 inches high, including the eight-inch vase, and it is about 16 inches in diameter."
+      },
+      MothersEmbraceLarge: {
+        say: "The large arrangement is about 19 inches high, including the eight-inch vase, and it is about 18 inches in diameter."
+      },
+      BirthdayDescription: {
+        say: "Hand-crafted arrangement of hot-pink roses, purple carnations, hot-pink mini carnations, yellow daisy poms, purple alstroemeria, athos poms, solidago and salal. Gathered in a stylish, 8-inch glass vase tied with colorful ribbon. It comes with an 18-inch Mylar birthday balloon."
+      },
+      BirthdaySmall: {
+        say: "The small arrangement is about 14 inches high, including the eight-inch vase, and it is about 10 inches in diameter."
+      },
+      BirthdayMedium: {
+        say: "The medium arrangement is about 15 inches high, including the eight-inch vase, and it is about 11.5 inches in diameter."
+      },
+      BirthdayLarge: {
+        say: "The large arrangement is about 16 inches high, including the eight-inch vase, and it is about 13 inches in diameter."
+      },
+      RomanceDescription: {
+        say: "This field-gathered bouquet has gorgeous red and pink roses, lilies, Gerbera daisies and alstroemeria, in an eight-inch glass vase tied with satin ribbon."
+      },
+      RomanceSmall: {
+        say: "The small arrangement is about 17 inches high, including the eight-inch vase, and it is about 13 inches in diameter."
+      },
+      RomanceMedium: {
+        say: "The medium arrangement is about 19 inches high, including the eight-inch vase, and it is about 14 inches in diameter."
+      },
+      RomanceLarge: {
+        say: "The large arrangement is about 20 inches high, including the eight-inch vase, and it is about 16 inches in diameter."
+      },
+      ThankYouDescription: {
+        say: "This colorful bouquet is the next best thing to a friendly hug! It is crafted by our expert florists from the hot-pink roses, Asiatic lilies, medium sunflowers, lavender stock, orange alstroemeria and athos poms in a classic cylinder vase."
+      },
+      ThankYouSmall: {
+        say: "The small arrangement is about 11 inches high, including the eight-inch vase, and it is about 11 inches in diameter."
+      },
+      ThankYouMedium: {
+        say: "The medium arrangement is about 12 inches high, including the eight-inch vase, and it is about 12 inches in diameter."
+      },
+      ThankYouLarge: {
+        say: "The large arrangement is about 13 inches high, including the eight-inch vase, and it is about 13 inches in diameter."
+      }
+    }
+  }
+
+Our florists hand-design each arrangement, so colors, varieties, and container may vary due to local availability
+Lilies may arrive in bud form and will open to full beauty over the next 2-3 days
