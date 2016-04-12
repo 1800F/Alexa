6381fe655e98ef25fac6e5f5a93fd8681b68c082
michael-krebs <michael@krb.io>
2016-04-12 14:12:31 -0400

    Add more line spacing on card

--- skill/responses.js
+++ skill/responses.js
@@ -271,8 +271,8 @@ var responses = function () {
           type: 'Standard',
           title: 'You bought flowers!',
           text: 'Your {arrangementSize} {arrangementType} order has been placed!\n' +
-            '{recipient}\n{address}\n' +
-            'Delivery Date: {deliveryDate}\n' +
+            '{recipient}\n{address}\n\n' +
+            'Delivery Date: {deliveryDate}\n\n' +
             'Order Total: {priceTextFormatted}',
           image: {
             smallImageUrl: '{imageUrl}',
