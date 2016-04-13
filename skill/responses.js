92cc0ea85443bbf984abd22c86041dfe19599e91
rainermatthewp <matthewp@rain.agency>
2016-04-13 12:15:02 -0600

    Update responses.js

--- skill/responses.js
+++ skill/responses.js
@@ -274,12 +274,12 @@ var responses = function () {
         card: {
           type: 'Standard',
           title: 'You bought flowers!',
-          text: 'Your {arrangementSize} {arrangementType} order has been placed!\n' +
-            '-\n' +
+          text: 'Your {arrangementSize} {arrangementType} flowers are on their way!\n' +
+            '   \n' +
             '{recipient}\n{address}\n' +
-            '-\n' +
+            '   \n' +
             'Delivery Date: {deliveryDate}\n' +
-            '-\n' +
+            '   \n' +
             'Order Total: {priceTextFormatted}',
           image: {
             smallImageUrl: '{imageUrl}',
