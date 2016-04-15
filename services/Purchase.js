41569e15169bf91ded2e3b016669a88a1721c988
Mark Miles <markm@mediarain.com>
2016-04-15 15:48:49 -0600

    removed comments

--- services/Purchase.js
+++ services/Purchase.js
@@ -516,9 +516,6 @@ var Purchase = module.exports= function Purchase(options,tokens) {
       return issue(method, token, path, queryString, body, options, apiType).then(function (res) {
         if (res.statusCode == 401) {
           //Our token expired
-          // if (!tokens.access_token) tokens.refresh_token = null; //Must have already tried a refresh, so this next time, go anew
-          // tokens.access_token = null;
-          // return apprequest.apply(self, args);
           return qAuthReq = oauthReq('password' ,options.defaultCredentials , options,'payment').then(function (toks) {
             qAuthReq = null;
             if(toks.error) return Promise.reject(toks.error);
