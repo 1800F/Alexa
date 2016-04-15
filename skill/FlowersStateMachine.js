ad929be9fc462d8b90b47998e4af7433c7876857
michael-krebs <michael@krb.io>
2016-04-15 12:45:51 -0400

    Revert "if ArrangementSelectionIntent is given at date selection, tell user how to select a date and reprompt"
    This reverts commit 062e5879a8e89dcf22a69876766ecc924135d409.

--- skill/FlowersStateMachine.js
+++ skill/FlowersStateMachine.js
@@ -109,8 +109,15 @@ module.exports = StateMachine({
     "launch": {
       enter: function enter(request) {
         return this.Access(request)
-        .then(PartialOrder.empty)
-        .then(function (po) {
+        .then(function(api) {
+          return PartialOrder.exists(request);
+        })
+        .then(function(exists) {
+          if (exists) {
+            return Promise.reject(StateMachineSkill.ERRORS.BAD_RESPONSE);
+          }
+          
+          var po = PartialOrder.empty();
           po.possibleRecipient = request.intent.params.recipientSlot;
           po.possibleDeliveryDate = request.intent.params.deliveryDateSlot;
           po.pickArrangement(request.intent.params.arrangementSlot);
@@ -127,6 +134,7 @@ module.exports = StateMachine({
             if (request.session.attributes.reply) {
               return replyWith(request.session.attributes.reply.msgPath, request.session.attributes.reply.state, request, po);
             }
+            return replyWith(null, 'launch', request, null);
           });
       }
     },
@@ -136,7 +144,7 @@ module.exports = StateMachine({
           .then(function(api){return PartialOrder.fromRequest(api,request); })
           .then(function(po){
             po = PartialOrder.empty();
-            return replyWith(null, 'launch', request,po);
+            return replyWith(null, 'options-review', request, po);
           });
       }
     },
@@ -313,13 +321,15 @@ module.exports = StateMachine({
         return this.Access(request)
         .then(function(api){ return PartialOrder.fromRequest(api,request); })
         .then(function(po){
-          if (verbose) console.log(request.intent.params);
-          return po.pickArrangement(request.intent.params.arrangementSlot).then(function (success) {
-            if (!success) {
-              return replyWith('Errors.ErrorGeneral', 'die', request, po);
-            }
-            return replyWith('ArrangementSelectionIntent.ArrangementValidation', 'options-review', request, po);
-          });
+          if (request.intent.params && request.intent.params.arrangementSlot) {
+            if (verbose) console.log('Arrangement Selection Params ',request.intent.params);
+            return po.pickArrangement(request.intent.params.arrangementSlot).then(function (success) {
+              if (!success) {
+                return replyWith('Errors.ErrorGeneral', 'die', request, po);
+              }
+              return replyWith('ArrangementSelectionIntent.ArrangementValidation', 'options-review', request, po);
+            });
+          }
         });
       }
     },
@@ -413,9 +423,6 @@ module.exports = StateMachine({
         return this.Access(request)
         .then(function(api){ return PartialOrder.fromRequest(api,request); })
         .then(function(po){
-          if (request.intent.name === 'ArrangementSelectionIntent') {
-            return replyWith('QueryDate.InvalidDate', 'query-date', request,po);
-          }
           if (request.intent.name == 'AMAZON.YesIntent') {
             if(po.deliverDateOffers && po.deliverDateOffers.length == 1) {
               po.acceptPossibleDeliveryDate(po.deliverDateOffers[0]);
